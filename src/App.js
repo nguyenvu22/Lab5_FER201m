14a93b053b4ec0087859039a258ed8c6fd5fa0a4
vupro123kute@gmail.com <vupro123kute@gmail.com>
2022-06-14 16:54:26 +0700

    hohoho merry christamasssss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+
+import Menu from "./components/MenuComponent";
+import { DISHES } from './shared/dishes';
+
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +27,29 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container text-left">
+            <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        {/* <Menu /> */}
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
